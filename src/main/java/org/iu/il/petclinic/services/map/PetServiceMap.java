4651375c522b6dce55d8b6ced94b9ee9f2eecef7
mert.il <mert.il@cyberinnovations.de>
2022-03-19 14:36:40 +0100

    implementing map based services

---
+++ src/main/java/org/iu/il/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package org.iu.il.petclinic.services.map;
+
+import org.iu.il.petclinic.models.Pet;
+import org.iu.il.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
